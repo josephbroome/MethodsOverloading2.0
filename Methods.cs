b6693989dfb259a9c0c32be9a74f8ee9781984e0
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-11 08:25:28 -0700

    methodsoverloading

---
+++ Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading2._0
+{
+    internal class Methods
+    {
+        public static double Add(double x, double y)
+        {
+
+            return x + y;
+
+        }
+
+        public static double Add(double x, double y, double z)
+        {
+            return x + y + z;
+
+
+        }
+
+
+        public static string Add(double num1, double num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using  MethodOverloading2._0;
+
+Console.WriteLine(Methods.Add(1, 0,true));
